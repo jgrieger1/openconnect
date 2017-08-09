9800b15ca052b1767a8276ebaa94fc7c6bbc6ec8
U-GRIEGERS\Jeff <Jeff@DSOM10.Griegers.com>
2017-08-09 16:46:11 -0400

    update gitignore.  Add build64.sh

---
+++ build64.sh
@@ -0,0 +1,29 @@
+rm -f -r build64
+mkdir build64
+mkdir ./build64/bin
+cd build64
+../configure
+make
+cd bin
+export MINGW_PREFIX=/mingw64
+
+cp ${MINGW_PREFIX}/bin/libffi-6.dll .
+cp ${MINGW_PREFIX}/bin/libgcc_*-1.dll .
+cp ${MINGW_PREFIX}/bin/libgmp-10.dll .
+cp ${MINGW_PREFIX}/bin/libgnutls-30.dll .
+cp ${MINGW_PREFIX}/bin/libhogweed-4.dll .
+cp ${MINGW_PREFIX}/bin/libiconv-2.dll .
+cp ${MINGW_PREFIX}/bin/libidn-11.dll .
+cp ${MINGW_PREFIX}/bin/libintl-8.dll .
+cp ${MINGW_PREFIX}/bin/liblz4.dll .
+cp ${MINGW_PREFIX}/bin/liblzma-5.dll .
+cp ${MINGW_PREFIX}/bin/libnettle-6.dll .
+cp ${MINGW_PREFIX}/bin/libp11-kit-0.dll .
+cp ${MINGW_PREFIX}/bin/libtasn1-6.dll .
+cp ${MINGW_PREFIX}/bin/libunistring-2.dll .
+cp ${MINGW_PREFIX}/bin/libwinpthread-1.dll .
+cp ${MINGW_PREFIX}/bin/libxml2-2.dll .
+cp ${MINGW_PREFIX}/bin/zlib1.dll .
+# cp ${MINGW_PREFIX}/bin/libstoken-1.dll .
+cp ../.libs/libopenconnect-5.dll .
+cp ../.libs/openconnect.exe .
